923e8549a223d196712b9ab23842160e39b369d7
wangjiguang <wangjiguang@tusvn.com>
2019-07-31 19:30:18 +0800

    Redis Jedis使用

--- redis/src/main/java/study/redis/JedisTest.java
+++ redis/src/main/java/study/redis/JedisTest.java
@@ -2,11 +2,55 @@ package study.redis;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
+import redis.clients.jedis.ScanParams;
+import redis.clients.jedis.ScanResult;
+import redis.clients.jedis.Tuple;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 public class JedisTest {
+
+    public void test() {
+
+
+        Vehicle vc = null;
+        Device dev = null;
+        for (int i=0; i<10; i++) {
+            vc = new Vehicle("vehicleId" + i, "vin" + i, "plateNo" + i);
+            dev = new Device("deviceId" + i, "deviceName" + i);
+            JedisUtil.getInstance().zadd("typeahead:vehicle", i,JSON.toJSONString(vc));// 有序集合
+            JedisUtil.getInstance().zadd("typeahead:device", i,JSON.toJSONString(dev));// 有序集合
+        }
+
+
+        //zrevrange
+        System.out.println("---------zrevrange---------");
+        Set<String> rev = JedisUtil.getInstance().zrevrange("typeahead:vehicle", 0, 2);
+        for (String str: rev) {
+            System.out.println(str);
+        }
+        JedisUtil.getInstance().zrem("typeahead:vehicle", JSON.toJSONString(vc));
+
+        //zscan
+        System.out.println("---------zscan---------");
+        String cur = "0";
+        ScanParams scanParams = new ScanParams();
+        scanParams.count(3);
+        List<Tuple> revlist = new ArrayList<Tuple>();
+        ScanResult<Tuple> scanResult = null;
+        do {
+            scanResult = JedisUtil.getInstance().zscan("vehicle", cur, scanParams.match("*\"vin\":\"*0*\"*"));
+            cur = scanResult.getStringCursor();
+            System.out.println("size=" + scanResult.getResult().size());
+            revlist.addAll(scanResult.getResult());
+        } while(!cur.equals("0"));
+
+        for (Tuple obj: revlist) {
+            System.out.println(obj.getScore() + "-->" + obj.getElement());
+        }
+    }
 
     public static void main(String[] args) {
 
--- redis/src/main/java/study/redis/JedisUtil.java
+++ redis/src/main/java/study/redis/JedisUtil.java
@@ -1,60 +1,42 @@
 package study.redis;
 
 import com.alibaba.fastjson.JSON;
-import redis.clients.jedis.Jedis;
-import redis.clients.jedis.ScanParams;
-import redis.clients.jedis.ScanResult;
-import redis.clients.jedis.Tuple;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.util.StringUtils;
+import redis.clients.jedis.*;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class JedisUtil {
-    private static Jedis redis;
+    private static JedisCluster jedisCluster;
 
-    public static Jedis getInstance() {
-        return redis==null?new Jedis("localhost", 6379):redis;
+    static {
+        ResourceBundle redisRb = ResourceBundle.getBundle("application");
+        String addr = redisRb.getString("redis.addr");
+        int total = Integer.parseInt(redisRb.getString("redis.total"));
+        int idle = Integer.parseInt(redisRb.getString("redis.idle"));
+        int maxWait = Integer.parseInt(redisRb.getString("redis.maxWait"));
+        String password = null; //redisRb.getString("reids.password");
+        JedisPoolConfig config = new JedisPoolConfig();
+        config.setMaxTotal(total);
+        config.setMaxIdle(idle);
+        config.setMaxWaitMillis(maxWait);
+        Set<HostAndPort> hostAndPorts = new HashSet<>();
+        for (String host : addr.split(",")) {
+            hostAndPorts.add(new HostAndPort(host.split(":")[0], Integer.parseInt(host.split(":")[1])));
+        }
+        if (password!=null && !"".equals(password.trim())) {
+            jedisCluster = new JedisCluster(hostAndPorts, 2000, 2000, 5, config);
+        } else {
+            jedisCluster = new JedisCluster(hostAndPorts, 2000, 2000, 5, password, config);
+        }
+    }
+
+    public static JedisCluster getInstance() {
+        return jedisCluster;
     }
 
     public static void main(String[] args) {
-
-        Vehicle vc = null;
-        Device dev = null;
-        for (int i=0; i<10; i++) {
-            vc = new Vehicle("vehicleId" + i, "vin" + i, "plateNo" + i);
-            dev = new Device("deviceId" + i, "deviceName" + i);
-            JedisUtil.getInstance().zadd("typeahead:vehicle", i,JSON.toJSONString(vc));// 有序集合
-            JedisUtil.getInstance().zadd("typeahead:device", i,JSON.toJSONString(dev));// 有序集合
-        }
-
-
-        //zrevrange
-        System.out.println("---------zrevrange---------");
-        Set<String> rev = JedisUtil.getInstance().zrevrange("typeahead:vehicle", 0, 2);
-        for (String str: rev) {
-            System.out.println(str);
-        }
-        JedisUtil.getInstance().zrem("typeahead:vehicle", JSON.toJSONString(vc));
-
-        //zscan
-        System.out.println("---------zscan---------");
-        String cur = "0";
-        ScanParams scanParams = new ScanParams();
-        scanParams.count(3);
-        List<Tuple> revlist = new ArrayList<Tuple>();
-        ScanResult<Tuple> scanResult = null;
-        do {
-            scanResult = JedisUtil.getInstance().zscan("vehicle", cur, scanParams.match("*\"vin\":\"*0*\"*"));
-            cur = scanResult.getStringCursor();
-            System.out.println("size=" + scanResult.getResult().size());
-            revlist.addAll(scanResult.getResult());
-        } while(!cur.equals("0"));
-
-        for (Tuple obj: revlist) {
-            System.out.println(obj.getScore() + "-->" + obj.getElement());
-        }
+        System.out.println(JedisUtil.getInstance().hget("vehicle_base_info", "B21E-00-021"));
     }
 }
